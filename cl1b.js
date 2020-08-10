9e9d7099665b7fe166d42d4be06124eba14fce0f
clonalejandro <clonalejandromcb@gmail.com>
2020-08-10 14:57:10 +0200

    Add cl1b

---
+++ cl1b.js
@@ -0,0 +1,7 @@
+module.exports = {
+    get: (server, id, callback, isApi = true) => !isApi ? server.get(id, callback) : server.get(`/api/${id}`, callback),
+    post: (server, id, callback) => server.post(`/api/${id}`, callback),
+    put: (server, id, callback) => server.put(`/api/${id}`, callback),
+    delete: (server, id, callback) => server.delete(`/api/${id}`, callback),
+    portHook: port => console.log(`Backend is up listening the port ${port} 🎨`)
+}
