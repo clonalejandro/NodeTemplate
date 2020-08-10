48c99a2e2ed933421289e7079211895134e6dbac
clonalejandro <clonalejandromcb@gmail.com>
2020-08-10 14:56:13 +0200

    Add action controller with examples

---
+++ actions.js
@@ -0,0 +1,30 @@
+module.exports = [
+    {
+        id: 'usuario',
+        type: 'get',
+        callback: (req, res) => {
+
+        }
+    },
+    {
+        id: 'usuario',
+        type: 'post',
+        callback: (req, res) => {
+
+        }
+    },
+    {
+        id: 'usuario',
+        type: 'put',
+        callback: (req, res) => {
+
+        }
+    },
+    {
+        id: 'usuario',
+        type: 'delete',
+        callback: (req, res) => {
+
+        }
+    }
+]
